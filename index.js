b415241bb86f99c5a5bc6f52680085a1fc6b3454
Ankit Singh <ankiitsingh21@gmail.com>
2024-12-17 20:07:54 +0530

    merge

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { connect } = require('./config/database.js');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/index.js');
-const { PORT } = require('./config/serverConfig.js');
+const  {PORT}  = require('./config/serverConfig.js');
 
 
 const app=express();
