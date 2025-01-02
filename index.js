4fe1466964538c729b4e1fc4f712f79b066c3b7f
Ankit Singh <ankiitsingh21@gmail.com>
2025-01-02 18:29:15 +0530

    improved

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  console.log('hello');
-});
+
 app.use('/api', apiRoutes);
 
 // app.post('/createuserinuserTableAndProfile',signUp,createProfile);
